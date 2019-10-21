86dd3d6b403f6e13925fff13a12f69cab0e5f29d
Marjorie <marjorie.jacquot21@gmail.com>
2019-10-21 15:55:45 +0200

    updateMarjorie

--- src/main/java/com/inti/formation/ProjetOpenGovApplication.java
+++ src/main/java/com/inti/formation/ProjetOpenGovApplication.java
@@ -9,6 +9,7 @@ public class ProjetOpenGovApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ProjetOpenGovApplication.class, args);
 		System.out.println("hello team !");
+		System.out.println("hello team team !");
 	}
 
 }
